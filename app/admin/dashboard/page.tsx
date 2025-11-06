bb6d6ef178e7995b144011700bed443bdefe82d4
Alhiefikri <alhiepedia@gmail.com>
2025-11-06 13:28:27 +0800

    feature: middleware configuration

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const DashboardPage = () => {
+  return <div className="py-20 mt-10">DashboardPage Component</div>;
+};
+
+export default DashboardPage;
---
+++ app/checkout/page.tsx
@@ -0,0 +1,5 @@
+const CheckoutPage = () => {
+  return <div className="py-20 mt-10">Checkout Component</div>;
+};
+
+export default CheckoutPage;
