032ae4a15bde00a29d881b77f2e73a401f7779a9
Alhiefikri <alhiepedia@gmail.com>
2025-11-06 23:43:22 +0800

    feature: create room and blob configuration

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "ukixzdmmhe6hycud.public.blob.vercel-storage.com",
+      },
     ],
   },
 };
