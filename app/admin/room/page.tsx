9357384875dc2b77ecb0d6d96c2f7dd7d33a9362
Alhiefikri <alhiepedia@gmail.com>
2025-11-14 08:44:52 +0800

    feature: delete button function

--- app/admin/room/page.tsx
+++ app/admin/room/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 
 const RoomPage = () => {
   return (
-    <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
+    <div className="max-w-7xl px-4 py-16 mt-10 mx-auto">
       <div className="flex items-center justify-between">
         <h1 className="text-4xl font-bold text-gray-800">Room List</h1>
         <Link
--- lib/actions.ts
+++ lib/actions.ts
@@ -3,6 +3,8 @@
 import { prisma } from "@/lib/prisma";
 import { ContactSchema, RoomSchema } from "@/lib/zod";
 import { redirect } from "next/navigation";
+import { del } from "@vercel/blob";
+import { revalidatePath } from "next/cache";
 
 export const saveRoom = async (
   image: string,
@@ -77,4 +79,17 @@ export const ContactMessage = async (
   } catch (error) {
     console.log(error);
   }
+};
+
+// Delete Room
+export const deleteRoom = async (id: string, image: string) => {
+  try {
+    await del(image);
+    await prisma.room.delete({
+      where: { id },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  revalidatePath("/admin/room");
 };
